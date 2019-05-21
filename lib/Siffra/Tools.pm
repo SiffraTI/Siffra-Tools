d00c3dba845bcb12a78bb8f5cc4d4914286e886a
Luiz Benevenuto <luiz@siffra.com.br>
2019-05-21 10:17:29 -0300

    :pencil: Small changes

--- lib/Siffra/Tools.pm
+++ lib/Siffra/Tools.pm
@@ -40,7 +40,7 @@ BEGIN
     require Siffra::Base;
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION = '0.06';
+    $VERSION = '0.07';
     @ISA     = qw(Siffra::Base Exporter);
 
     #Give a hoot don't pollute, do not export more than needed by default
